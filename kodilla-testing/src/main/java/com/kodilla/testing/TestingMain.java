d86c7d356d10f14bdc40780f550122be8efad959
Marcin Suder <marcin.code.github@gmail.com>
2025-12-10 22:21:39 +0100

    add first unit tests for Calculator - add and subtract

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        // testing SimpleUser
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // testing Calculator
+        Calculator calculator = new Calculator();
+
+        int sumResult = calculator.add(5, 10);
+        int subtractResult = calculator.sub(5, 10);
+
+        if (sumResult == 15) {
+            System.out.println("test OK");
+        }else  {
+            System.out.println("Error!");
+        }
+
+        if (subtractResult == -5) {
+            System.out.println("test OK");
+        }else{
+            System.out.println("Error!");
+        }
+
     }
 }
