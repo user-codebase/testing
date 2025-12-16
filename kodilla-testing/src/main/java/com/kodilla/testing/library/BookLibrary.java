26636814800dc4d9d4ab690e3c81cf70be5dadc5
Marcin Suder <marcin.code.github@gmail.com>
2025-12-16 17:55:08 +0100

    add implementation to BookLibrary method - 'listBooksInHandsOf'

--- kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -22,10 +22,6 @@ public class BookLibrary {
     }
 
     public List<Book> listBooksInHandsOf(LibraryUser libraryUser){
-        List<Book> bookList = new ArrayList<>();
-
-        // to be implemented
-
-        return bookList;
+        return libraryDatabase.listBooksInHandsOf(libraryUser);
     }
 }
